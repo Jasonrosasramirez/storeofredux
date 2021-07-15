9726cdebbc51d863942b49dfe9f3359d10230fbe
Jason <jrosasrjf@gmail.com>
2021-07-14 21:47:17 -0500

    working out git issues

---
+++ client/build/precache-manifest.bb77a70eca463a52eaaec2e902dc7609.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a06c749e44bff35942ec2afb83199b91",
+    "url": "/index.html"
+  },
+  {
+    "revision": "535c64b2997d01159e11",
+    "url": "/static/css/main.a64bd9e7.chunk.css"
+  },
+  {
+    "revision": "632cf3eee9e0e3c83e01",
+    "url": "/static/js/2.2bd94e0d.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/static/js/2.2bd94e0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "535c64b2997d01159e11",
+    "url": "/static/js/main.63711a13.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
